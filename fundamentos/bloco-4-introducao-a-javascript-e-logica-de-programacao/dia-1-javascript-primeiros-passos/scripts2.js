21f5a540a4ece3d14bd20b0b13ed7fb9a5105adf
Caique Silva <caiquesilva12@hotmail.com>
2022-05-12 22:43:23 -0300

    Exercícios de introdução à JavaScript e Lógica de Programação

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts2.js
@@ -0,0 +1,8 @@
+const a = 5
+const b = 10
+
+if (a > b) {
+  console.log('a é maior que b')
+} else {
+  console.log('b é maior que a')
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts4.js
@@ -0,0 +1,9 @@
+const a = -4
+
+if (a > 0) {
+  console.log('o número é positivo')
+} else if (a < 0) {
+  console.log('o número é negativo')
+} else {
+  console.log('o número é zero')
+}
