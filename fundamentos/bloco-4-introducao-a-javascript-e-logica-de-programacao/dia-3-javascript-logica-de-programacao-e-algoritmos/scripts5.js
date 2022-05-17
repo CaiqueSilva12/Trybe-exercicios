cbc232f348fedb6dcc63d27b399e4c49c73c3c80
Caique Silva <caiquesilva12@hotmail.com>
2022-05-17 11:43:05 -0300

    Exercícios bônus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts5.js
@@ -0,0 +1,12 @@
+{
+  let quadradoDeAsterisco = ''
+  let asterisco = '*'
+  let n = 5
+
+  for (let index = 0; index < n; index++) {
+    quadradoDeAsterisco = quadradoDeAsterisco + asterisco
+  }
+  for (let index = 0; index < n; index++) {
+    console.log(quadradoDeAsterisco)
+  }
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts6.js
@@ -0,0 +1,10 @@
+{
+  let quadradoDeAsterisco = ''
+  let asterisco = '*'
+  let n = 5
+
+  for (let index = 0; index < n; index++) {
+    quadradoDeAsterisco = quadradoDeAsterisco + asterisco
+    console.log(quadradoDeAsterisco)
+  }
+}
